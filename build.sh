5dd30e177888fd1c45f8a52e8322e7c163d64013
commit-person <commit-person@localhost>
2019-07-14 11:48:31 +0100

    use shards to build

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-targets=(tosm ysnp zenv)
-
-for target in ${targets[@]}; do
-    echo "Building $target"
-    crystal build $target.cr
-    echo "Linking $target"
-    ln -fs $(realpath $target) $HOME/bin
-done
