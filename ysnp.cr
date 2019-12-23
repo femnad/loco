78a456a94ebec3fbb75f92cbab09fbe3d4b5698e
fcd <femnad@users.noreply.github.com>
2019-12-23 17:56:22 +0300

    add password type operation

--- ysnp.cr
+++ ysnp.cr
@@ -1,9 +1,12 @@
-require "yaml"
-
 NAME = "ysnp"
 MENU_DISPLAYER = "profit"
 PRINCIPAL_SPECIFIER = "login"
 PRINCIPAL_PRIORITIES = ["email", "username"]
+
+def message_and_exit(msg)
+  puts msg
+  exit 1
+end
 
 def get_secret_lines(pass_name)
   `pass show #{pass_name}`.strip.split('\n')
@@ -15,16 +18,18 @@ end
 
 def get_principal(pass_aux_lines)
   if pass_aux_lines.size == 0
-    raise "No principal in password"
+    puts "No principal in password"
+    exit 1
   end
 
   if pass_aux_lines.size == 1
     return pass_aux_lines[0].split(": ")[-1]
   end
 
-  doc = pass_aux_lines.join('\n')
-  parsed = YAML.parse(doc)
-  pass_info = parsed.as_h
+  pass_info = {} of String => String
+  pass_aux_lines.each{ |line|
+      key, value = line.split(": ")
+  }
 
   if pass_info.has_key?(PRINCIPAL_SPECIFIER)
     puts pass_info
@@ -39,7 +44,7 @@ def get_principal(pass_aux_lines)
     end
   }
 
-  raise "Cannot determine principal"
+  message_and_exit "Cannot determine principal"
 end
 
 def get_principal_and_password(pass_name)
@@ -49,35 +54,58 @@ def get_principal_and_password(pass_name)
   return principal, password
 end
 
-def perform_operation(operation, pass_secret)
-  case operation
-  when "copy"
+def copy_password(pass_secret)
     `pass -c #{pass_secret}`
-  when "type"
+end
+
+def type_password(pass_secret)
     password = get_password(pass_secret)
     `xdotool type '#{password}'`
-  when "login-tab-pass"
+end
+
+def type_login_and_password(pass_secret)
     principal, password = get_principal_and_password(pass_secret)
     `xdotool type #{principal}`
     `xdotool key Tab`
     `xdotool type '#{password}'`
-  else
-    raise "Unrecognised operation #{operation}"
-  end
 end
 
+def type_login(pass_secret)
+    principal, _ = get_principal_and_password(pass_secret)
+    `xdotool type #{principal}`
+end
+
+OPERATION_MAP = {
+    "copy" => ->copy_password(String),
+    "login-tab-pass" => ->type_login_and_password(String),
+    "login" => ->type_login(String),
+    "type" => ->type_password(String),
+}
+
+def valid_operations
+    OPERATION_MAP.keys.join(", ")
+end
+
+def perform_operation(operation)
+    pass_operation = OPERATION_MAP.fetch(operation, nil)
+    if pass_operation.nil?
+        message_and_exit "Unrecognised operation #{operation}\nNeeded one of #{valid_operations}"
+    end
+    secret = `#{MENU_DISPLAYER}`
+    pass_operation.call(secret)
+end
+
+
 def usage
-  puts "usage: #{NAME} <operation>"
+  puts "usage: #{NAME} <operation>\n    where operation is one of #{valid_operations}"
 end
 
 def get_operation(args)
   if args.size != 1
-    usage
-    exit 1
+      message_and_exit usage
   end
   operation = args[0]
-  secret = `#{MENU_DISPLAYER}`
-  perform_operation(operation, secret)
+  perform_operation(operation)
 end
 
 get_operation(ARGV)
