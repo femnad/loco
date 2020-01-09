c5164cb2f5c831942cb62ee1d6d2c824a1af4129
fcd <femnad@users.noreply.github.com>
2020-01-09 14:47:22 +0300

    add subcommand

--- clom.cr
+++ clom.cr
@@ -1,6 +1,35 @@
+require "admiral"
+
 CLONE_PATH="~/z/gl"
 PROG="clom"
-REPO_REGEX=%r<(^https|git)://git(hub|lab)\.com/[a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+(\.git)?$>
+REPO_REGEX=%r<(^https|git)://git(hub|lab)\.com/[a-zA-Z0-9_.-]+/[a-zA-Z0-9_.-]+(\.git)?$>
+
+class Clom < Admiral::Command
+    class CloneLoop < Admiral::Command
+        def run
+            clone_loop
+        end
+    end
+
+    class OneShot < Admiral::Command
+
+        define_argument repo : String, required: true
+
+        def run
+            clone_if_git_repo arguments.repo
+        end
+    end
+
+    define_help description: "clom: monitor clipboard for repos to clone"
+    define_version "0.5.0"
+
+    register_sub_command clone_loop : CloneLoop, description: "Run the clone loop"
+    register_sub_command one_shot : OneShot, description: "Clone if the argument is a repo"
+
+    def run
+        puts help
+    end
+end
 
 def notify(msg)
     `notify-send #{PROG} '#{msg}'`
@@ -22,17 +51,18 @@ def clone_if_git_repo(item)
     clone_path = Path[CLONE_PATH].expand(home: ENV["HOME"])
     repo_path = "#{clone_path.to_s}/#{basename}"
 
-    if File.exists?(repo_path) && File.directory?(repo_path)
+    if File.directory?(repo_path)
         Dir.cd(repo_path)
         notify "Updating #{repo}"
         `git fetch`
     else
         notify "Cloning #{repo}"
-        `git clone #{repo} #{repo_path}`
+        `git clone -q #{repo} #{repo_path}`
     end
 end
 
 def clone_loop
+    puts "Started clone loop"
     loop do
         `clipnotify`
         clipboard_item=`xclip -o`
@@ -40,4 +70,4 @@ def clone_loop
     end
 end
 
-clone_loop
+Clom.run
